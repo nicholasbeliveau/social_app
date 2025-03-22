067b1a328861978f7d473bfb135713844bc79d77
Nick Beliveau <nicholasbeliveau@gmail.com>
2025-03-22 16:30:57 -0400

    Fix error using suppressHydrationWarning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { SignedOut } from "@clerk/clerk-react";
 import { ThemeProvider } from "@/components/ThemeProvider";
+import Navbar from "@/components/Navbar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,7 +30,7 @@ export default function RootLayout({
   return (
 
     <ClerkProvider>
-      <html lang="en">
+      <html lang="en" suppressHydrationWarning>
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
 
         <ThemeProvider
@@ -38,7 +39,27 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
-          {children}
+
+          <div className="min-h-screen">
+            <Navbar/>
+
+            <main className="py-8">
+              {/* container to center the content */}
+              <div className="max-w-7xl mx-auto px-4">
+                <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
+                  <div className="hidden lg:block lg:col-span-3">
+                    sidebar
+                  </div>
+                  <div className="lg:col-span-9">
+                    {children}
+                  </div>
+
+                </div>
+              </div>
+            </main>
+            
+          </div>
+          
         </ThemeProvider>
         </body>
       </html>
