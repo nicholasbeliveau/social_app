e85ed3aff804c2eac46edf41fc63c1b25638928b
Nick Beliveau <nicholasbeliveau@gmail.com>
2025-03-24 00:03:40 -0400

    Add Sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { SignedOut } from "@clerk/clerk-react";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
+
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -48,7 +50,7 @@ export default function RootLayout({
               <div className="max-w-7xl mx-auto px-4">
                 <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
                   <div className="hidden lg:block lg:col-span-3">
-                    sidebar
+                    <Sidebar/>
                   </div>
                   <div className="lg:col-span-9">
                     {children}
