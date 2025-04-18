7bad7e2cbe681526f53fb215206be870aebe6539
Nick Beliveau <nicholasbeliveau@gmail.com>
2025-04-18 12:24:43 -0400

    Add Hockey Tab to navbar and remove tasks

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,7 +17,7 @@ async function Navbar() {
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
             <Link href="/" className="text-xl font-bold text-primary font-mono tracking-wider">
-              Socially
+              Chirper
             </Link>
           </div>
 
