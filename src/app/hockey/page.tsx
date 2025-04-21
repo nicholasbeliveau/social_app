8c7039d167b88528bea2f22588ee1d32376f23bd
Nick Beliveau <nicholasbeliveau@gmail.com>
2025-04-21 19:09:07 -0400

    Move key to correct element and add today's date

--- src/app/hockey/page.tsx
+++ src/app/hockey/page.tsx
@@ -8,17 +8,17 @@ export default async function HockeyPage() {
   const data = await getLocalData();
 
   //getScores()
-  console.log( data );
-  const date = new Date( data[0].date );
+
+  const todaysDate = new Date( data[0].date );
+
   return (
 
     <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
       { data.map( (item) => (
-        <div className="lg:col-span-6">
-          <GameCard key={item.id} game={item} />
+        <div key={item.id} className="lg:col-span-6">
+          <GameCard game={item} />
         </div>
       ))}
     </div>
-
   );
 }
